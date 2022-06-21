e0cf182e0b7256d71fd6c914a71f7b5b13f8ac8b
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-21 11:20:23 -0300

    Add: real-time date and time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,26 +4,65 @@ class CalcController {
 
         /*O comando "this" referencia atributos e métodos. 
         Sendo atributos - váriaveis, e métodos - funções*/
-        this._displayCalc = "0";
-        this._dataAtual;
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize() {
+
+        setInterval(()=> {
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+        }, 1000);
 
     }
 
     /*getters e setters permitem definir como acessar os valores*/
+    get displayTime() {
+
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value) {
+
+        this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate() {
+
+        return this._dateEl.innerHTML;
+
+    }
+
+    set displayDate(value) {
+
+        this._dateEl.innerHTML = value;
+
+    }
+
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor) {
-        this._displayCalc = valor;
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual() {
-        return this._dataAtual;
+    get currentDate() {
+        return new Date();
     }
 
-    set dataAtual(valor) {
-        this._dataAtual = valor;
+    set currentDate(value) {
+        this._currentDate = value;
     }
 
 }
