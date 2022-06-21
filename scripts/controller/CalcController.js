0a95f9b7c65f7d3375c64243125bfea7d34e6653
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-21 11:34:44 -0300

    Add: Real-time date and time enhancement

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -15,12 +15,20 @@ class CalcController {
 
     initialize() {
 
+        this.setDisplayDateTime();
+
         setInterval(()=> {
 
-            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+            this.setDisplayDateTime();
 
         }, 1000);
+
+    }
+
+    setDisplayDateTime() {
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
 
     }
 
