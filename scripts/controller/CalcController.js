f26c762291ed0ba8e8dede212189c5f173029365
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-22 15:56:20 -0300

    Add: appearance of the number on the display, right after pressing the button

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -69,6 +69,47 @@ class CalcController {
 
     }
 
+    pushOperation(value) {
+
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
+        }
+
+    }
+
+    calc() {
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+    }
+
+    setLastNumberToDisplay() {
+
+        let lastNumber;
+
+        for (let i = this._operation.length - 1; i >= 0; i--){
+
+            if (!this.isOperator(this._operation[i])) {
+
+                lastNumber = this._operation[i];
+                break;
+
+            }
+
+        }
+
+        this.displayCalc = lastNumber;
+
+    }
+
     addOperation(value) {
 
         if (isNaN(this.getLastOperation())) {
@@ -79,11 +120,13 @@ class CalcController {
 
             } else if (isNaN(value)) {
 
-                console.log(value);
+                console.log('Outra coisa', value);
 
             } else {
 
-                this._operation.push(value);
+                this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
 
             }
 
@@ -91,18 +134,18 @@ class CalcController {
 
             if (this.isOperator(value)) {
 
-                this._operation.push(value);
+                this.pushOperation(value);
 
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
                 this.setLastOperation(parseInt(newValue));
 
+                this.setLastNumberToDisplay();
+
             }
 
         }
-
-        console.log(this._operation);
 
     }
 
