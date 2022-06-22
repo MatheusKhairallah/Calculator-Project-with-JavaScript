6f23ee5340153d8b46f59488e171e81597f1397d
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-22 17:57:11 -0300

    Add: working point button and bug fix on AC button

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -47,6 +47,10 @@ class CalcController {
 
         this._operation = [];
 
+        this._lastNumber = '';
+        
+        this._lastOperation = '';
+
         this.setLastNumberToDisplay();
 
     }
@@ -184,10 +188,6 @@ class CalcController {
 
                 this.setLastOperation(value);
 
-            } else if (isNaN(value)) {
-
-                console.log('Outra coisa', value);
-
             } else {
 
                 this.pushOperation(value);
@@ -205,7 +205,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -218,6 +218,24 @@ class CalcController {
     setError() {
 
         this.displayCalc = "Error";
+
+    }
+
+    addDot() {
+
+        let lastOperation = this.getLastOperation();
+
+        if (this.isOperator(lastOperation) || !lastOperation) {
+
+            this.pushOperation('0.');
+
+        } else {
+
+            this.setLastOperation(lastOperation.toString() + '.')
+
+        }
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -258,7 +276,7 @@ class CalcController {
             break
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot();
             break;
 
             case '0':
