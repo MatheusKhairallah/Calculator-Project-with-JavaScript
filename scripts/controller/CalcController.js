a43934f090fbf39f8bf26859913c1abb25805838
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-23 10:04:08 -0300

    Add: set the maximum value of numbers on the display screen

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -485,7 +485,17 @@ class CalcController {
     }
 
     set displayCalc(value) {
+
+        if (value.toString().length > 10) {
+
+            this.setError();
+
+            return false;
+
+        }
+
         this._displayCalcEl.innerHTML = value;
+
     }
 
     get currentDate() {
